e6ce8aa6177cd5d8bc7657b1fec7cd7eea172c6e
munal777 <munalpoudel3@gmail.com>
2025-12-02 08:55:05 +0545

    feat: complete the Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rect1 = new Rectangle();
+        double l = 3;
+        double b = 2;
+        rect1.Length = l;
+        rect1.Breadth = b;
+
+        Console.WriteLine(rect1.ShowDetails());
+        Console.WriteLine($"Area: {rect1.GetArea()}");
+        Console.WriteLine($"Perimeter: {rect1.GetPerimeter()}");
+    }
+}
