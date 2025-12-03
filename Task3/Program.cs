fdd5bd1015cb1f2210f1d1dede40fd45ffba506d
munal777 <munalpoudel3@gmail.com>
2025-12-03 09:36:27 +0545

    feat: completed the Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+
+    public static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+    {
+        foreach (var n in numbers)
+        {
+            if (condition(n))
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int[] nums = { 2, 5, 12, 8, 15, 20 };
+
+        ProcessNumbers(nums, n => n % 2 == 0);
+        Console.WriteLine("num 2");
+
+        ProcessNumbers(nums, n => n > 10);
+       
+
+    }
+
+
+
+
+}
